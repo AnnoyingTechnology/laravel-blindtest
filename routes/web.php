5425a2944806952b6bbbaf79748d93fa702fed96
AnnoyingTechnology <nospam.github1@gribouille.eu>
2024-07-26 13:46:54 +0200

    Softer throttling

--- routes/web.php
+++ routes/web.php
@@ -25,11 +25,11 @@ Route::get('/chat', function () {
 Route::post('/chat/send-message', [ChatController::class, 'store'])
     ->name('chat.send')
     ->middleware('auth')
-    ->middleware(['throttle:30,1,5,1']);
+    ->middleware(['throttle:45,1,10,1']);
 
 // streaming of a track audio file
 Route::get('/track/{id}', [TrackController::class, 'getTrack'])
 	->name('track')
     ->middleware('auth')
 	->middleware('signed')
-    ->middleware(['throttle:10,1']);
+    ->middleware(['throttle:15,1']);
